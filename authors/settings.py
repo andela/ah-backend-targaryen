05e7252017c84564b208d50d402dfb57f6fea8e9
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-07-20 09:50:12 +0300

    Remove default authentications

--- authors/settings.py
+++ authors/settings.py
@@ -136,8 +136,5 @@ AUTH_USER_MODEL = 'authentication.User'
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
-
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authors.apps.authentication.backends.JWTAuthentication',
-    ),
+    
 }

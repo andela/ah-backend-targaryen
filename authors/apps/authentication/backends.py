01bffa90333827cff7e0bb418d33c5956b9e7eda
David Muhanguzi <38077368+MuhanguziDavid@users.noreply.github.com>
2018-10-05 21:39:25 +0300

    bug(authentication): fix jwt decode method (#17)
    - add jwt authentication class to decode a token
    - add expiry time to jwt token
    - use secret key from configuration to encode jwt
    
    [Fixes #160986722]

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,10 +1,52 @@
-# import jwt
-#
-# from django.conf import settings
-#
-# from rest_framework import authentication, exceptions
-#
-# from .models import User
+import jwt
+from decouple import config
+from django.conf import settings
+from rest_framework import authentication, exceptions
+from rest_framework.authentication import (
+    BaseAuthentication, get_authorization_header)
 
-"""Configure JWT Here"""
+from .models import User
 
+
+class JWTAuthentication(BaseAuthentication):
+    """Authentication class called in views"""
+    authentication_header_name = 'Token'
+
+    def authenticate(self, request):
+        """
+        Splits the token header and
+        passes the result to the _authenticate_data method
+        """
+        request.user = None
+
+        auth_header = authentication.get_authorization_header(request).split()
+
+        if not auth_header:
+            return None
+
+        name = auth_header[0].decode('utf-8')
+        token = auth_header[1].decode('utf-8')
+
+        return self._authenticate_data(request, token)
+
+    def _authenticate_data(self, request, token):
+        """
+        Authenticates data received from the authenticate method and
+        returns a token to the user
+        """
+        try:
+            result = jwt.decode(token, config('SECRET_KEY'))
+        except:
+            message = 'Failed to decode token'
+            raise exceptions.AuthenticationFailed(message)
+
+        try:
+            user = User.objects.get(username=result['name'])
+        except User.DoesNotExist:
+            message = 'No user was found'
+            raise exceptions.AuthenticationFailed(message)
+
+        if not user.is_active:
+            message = 'This user has been deactivated.'
+            raise exceptions.AuthenticationFailed(message)
+        return (user, token)
--- authors/apps/authentication/views.py
+++ authors/apps/authentication/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from authors.apps.authentication.backends import JWTAuthentication
 from .renderers import UserJSONRenderer
 from .serializers import (
     LoginSerializer, RegistrationSerializer, UserSerializer

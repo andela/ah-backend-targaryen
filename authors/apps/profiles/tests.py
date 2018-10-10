60b1705b422263fab71e723abe6ad4babfc33b0c
David Muhanguzi <38077368+MuhanguziDavid@users.noreply.github.com>
2018-10-10 20:59:05 +0300

    bug(urls): fix missing slash on url (#23)
    - adds a missing slash at the end of the profiles retrieval url
    
    [Fixes #161107802]

--- authors/apps/profiles/tests.py
+++ authors/apps/profiles/tests.py
@@ -37,7 +37,7 @@ class ViewTestCase(TestCase):
     def test_profile_retreival(self):
         """Tests that a profile is created and retreived"""
         response = self.client.get(
-            '/api/profiles/{}'.format(self.username))
+            '/api/profiles/{}/'.format(self.username))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_profile_update(self):

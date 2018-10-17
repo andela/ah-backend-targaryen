e50e8eae1afd92c1491692e1b14fb1125958b9e2
Samuel Bwambale <31615608+samuelbwambale@users.noreply.github.com>
2018-10-17 16:39:23 +0300

    feature(tagging): create tags for articles (#26)
    - Ensure that a user can create tags for the articles that he/she authors
    - Get all tags
    
    [Delivers #160609564]

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,18 +1,38 @@
 from rest_framework import serializers
 
-from .models import Article
+from .models import (
+    Article,
+    Tag
+)
+from .relations import TagRelatedField
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     """Create a new article"""
 
+    tagList = TagRelatedField(many=True, required=False, queryset=Tag.objects.all(), source='tags')
+
     class Meta:
         model = Article
         fields = [
             'author', 'title', 'description', 'body',
-            'createdAt', 'updatedAt', 'slug'
+            'createdAt', 'updatedAt', 'slug', 'tagList'
         ]
 
     def create(self, validated_data):
         author = self.context.get('author', None)
-        return Article.objects.create(author=author, **validated_data)
+        tags_data = validated_data.pop('tags', [])
+        article = Article.objects.create(author=author, **validated_data)
+        for tag in tags_data:
+            article.tags.add(tag)
+        return article
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ('tag',)
+
+    def to_representation(self, value):
+        return value.tag
--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CreateArticle, ArticleRetrieveUpdate, ArticleList
+from .views import CreateArticle, ArticleRetrieveUpdate, ArticleList, TagList
 
 urlpatterns = [
     path('articles/', CreateArticle.as_view()),
     path('articles/<str:slug>/', ArticleRetrieveUpdate.as_view()),
-    path('article/', ArticleList.as_view())
+    path('article/', ArticleList.as_view()),
+    path('tags/', TagList.as_view())
 ]
